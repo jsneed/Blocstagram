565701797459f4fc69c8ac2ceb878b10acac08ea
Jeanne Sneed <jsneed@gmail.com>
2014-07-22 10:45:03 -0700

    Initial Commit

---
+++ Blocstagram/BLCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BLCAppDelegate.h
+//  Blocstagram
+//
+//  Created by Jeanne on 7/22/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Blocstagram/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Blocstagram
+//
+//  Created by Jeanne on 7/22/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BLCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BLCAppDelegate class]));
+    }
+}
